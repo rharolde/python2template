cdaaa4760b79387ffd84be45c07f544a764ab611
Bob Harold <rharolde@umich.edu>
2018-04-11 14:49:36 -0400

    Initial template for a python 2 script with placeholders and if __main__

---
+++ python2template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+
+''' title   QQQQ
+Copyright (c) QQQQ
+Author QQQQ
+Date QQQQ
+
+Description QQQQ
+
+python2template
+Replace all occurances of QQQQ
+with the appropriate information
+
+2018/04/11  rharolde@umich.edu
+'''
+
+def QQQQ():
+	print "function QQQQ"
+
+def main():
+	print "QQQQ"
+
+if __name__ == "__main__":
+	main()

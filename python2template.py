3e809a63b3294d845efc4dc45eddf686693bb4bd
Bob Harold <rharolde@umich.edu>
2018-04-19 16:16:25 -0400

    Add docstrings to functions. Edit to pass pylint.

--- python2template.py
+++ python2template.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2
 
-''' title   QQQQ
+''' one-line description QQQQ
+
+(Note blank line above for docstring)
+title QQQQ
 Version QQQQ
 Copyright (C) QQQQ
 Author QQQQ
@@ -12,13 +15,17 @@ python2template
 Replace all occurances of QQQQ
 with the appropriate information
 
+This template passes pylint
+
 2018/04/11  rharolde@umich.edu
 '''
 
-def QQQQ():
+def my_qqqq():
+    '''an example function QQQQ'''
     print "function QQQQ"
 
 def main():
+    '''docstring for main QQQQ'''
     print "QQQQ"
 
 if __name__ == "__main__":
